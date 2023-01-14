ba714ec96afd18abc8cc0875f6c36c5a391db73d
deekshith991 <109906616+deekshith991@users.noreply.github.com>
2023-01-14 23:08:44 +0530

    Delete .config/Min/userscripts directory

--- .config/Min/userscripts/freestance.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name Redirect to invidious
-// @match https*://youtube.com
-// @match https*://www.youtube.com
-// @run-at document-start
-// ==/UserScript==
-
-window.location.hostname = "invidious.snopyta.org";
